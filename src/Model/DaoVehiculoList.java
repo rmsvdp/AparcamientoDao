b154bcaff3991a8cdcdfe030b203a5217b80ccb8
rmsvdp <rmsvdp2022@gmail.com>
2025-03-06 19:11:13 +0100

    DaoVehiculoList v1

--- src/Model/DaoVehiculoList.java
+++ src/Model/DaoVehiculoList.java
@@ -14,33 +14,62 @@ public class DaoVehiculoList implements DaoList<Vehiculo>{
 	
 	public ArrayList<Vehiculo> findAll() {
 		// -------
-		return null;
+		// ----- devolver una copia
+		//return new ArrayList<Vehiculo>(listaVehiculos);
+		//  --- Referencia
+		return listaVehiculos;
 	}
-	public Vehiculo findOne(Integer key) {
+	public Vehiculo findOne(String key) {
 		
 		Vehiculo result = null;
+		
+		for(Vehiculo v : listaVehiculos) {
+			
+			if ( v.getMatricula().equals(key)) return v;
+		}
 
 		return result;
 	}
 	public boolean insertOne(Vehiculo v) {
 		
 		boolean result = false;
+		listaVehiculos.add(v);
+		result = true;
 
 		return result;
 	}
-	public boolean deleteOne(Integer key){
+	public boolean deleteOne(String key){
 		
 		boolean result = false;
 		
-
+			for(Vehiculo v : listaVehiculos) {
+				
+				if ( v.getMatricula().equals(key)) {
+					listaVehiculos.remove(v);
+					// return true;  // esto sería otra forma alternativa
+					result= true;
+					break;
+					
+				}
+			} // iterar
+		
 		return result;
 	}
 	
-	public boolean updateOne(Integer key,Vehiculo v){
+	public boolean updateOne(String key,Vehiculo v){
 		
-		boolean result = false;
-		
+		boolean result = false;		
 		//--- 
+		for(Vehiculo v1 : listaVehiculos) {
+			
+			if ( v1.getMatricula().equals(key)) {
+				listaVehiculos.set(listaVehiculos.indexOf(v1), v);
+				// return true;  // esto sería otra forma alternativa
+				result= true;
+				break;
+			}
+		} // iterar
+		
 		return result;
 	}
 
